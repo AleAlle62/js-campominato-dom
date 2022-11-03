60907948e66e08e5041152470c99b1e2f6d090c5
AleAlle62 <basketallegrini@gmail.com>
2022-11-03 16:13:15 +0100

    last

--- js /script.js
+++ js /script.js
@@ -34,90 +34,112 @@ elePlay.addEventListener('click', function(){
 })
 
 
-// // DIFFICOLTA 1
+// DIFFICOLTA 1
 
-// uno.addEventListener('click', function () {
+uno.addEventListener('click', function () {
 
-//     // ciclo per tutti i quadrati
-//     for (let i = 1; i < 101; i++) {
+    // ciclo per tutti i quadrati
+    for (let i = 1; i < 101; i++) {
             
-//         // creazione elemento HTML per le celle 
-//         const eleCell = document.createElement('div');
+        // creazione elemento HTML per le celle 
+        const eleCell = document.createElement('div');
         
-//         // creazione di un elemento nuovo
-//         eleCell.classList.add('cell');
+        // creazione di un elemento nuovo
+        eleCell.classList.add('cell');
             
-//         // mettere un contenuto all'interno di un elemento
-//         eleGrid.append(eleCell);
+        // mettere un contenuto all'interno di un elemento
+        eleGrid.append(eleCell);
             
-//         // stampa dei numeri 
-//         eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
+        // stampa dei numeri 
+        eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
         
-//         // funzione per cui se cliccli si colora collegato a CSS
-//         eleCell.addEventListener('click', function () {
-//             this.classList.toggle('active')
-//         });
-//     }
+        // funzione per cui se cliccli si colora collegato a CSS
+        eleCell.addEventListener('click', function () {
+            this.classList.toggle('active')
+        });
+    }
     
-// });
+});
 
 
 
 
-// //DIFFICOLTA 2
+//DIFFICOLTA 2
 
-// due.addEventListener('click', function () {
+due.addEventListener('click', function () {
 
-//     // ciclo per tutti i quadrati
-//     for (let i = 1; i < 81; i++) {
+    // ciclo per tutti i quadrati
+    for (let i = 1; i < 81; i++) {
             
-//         // creazione elemento HTML per le celle 
-//         const eleCell = document.createElement('div');
+        // creazione elemento HTML per le celle 
+        const eleCell = document.createElement('div');
         
-//         // creazione di un elemento nuovo
-//         eleCell.classList.add('cell');
+        // creazione di un elemento nuovo
+        eleCell.classList.add('cell');
             
-//         // mettere un contenuto all'interno di un elemento
-//         eleGrid.append(eleCell);
+        // mettere un contenuto all'interno di un elemento
+        eleGrid.append(eleCell);
             
-//         // stampa dei numeri 
-//         eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
+        // stampa dei numeri 
+        eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
         
-//         // funzione per cui se cliccli si colora collegato a CSS
-//         eleCell.addEventListener('click', function () {
-//             this.classList.toggle('active')
-//         });
-//     }
-// });
+        // funzione per cui se cliccli si colora collegato a CSS
+        eleCell.addEventListener('click', function () {
+            this.classList.toggle('active')
+        });
+    }
+});
 
 
 
 
-// //DIFFICOLTA 3
+//DIFFICOLTA 3
 
-// tre.addEventListener('click', function () {
+tre.addEventListener('click', function () {
 
-//         // ciclo per tutti i quadrati
-//     for (let i = 1; i < 49; i++) {
+        // ciclo per tutti i quadrati
+    for (let i = 1; i < 49; i++) {
             
-//         // creazione elemento HTML per le celle 
-//         const eleCell = document.createElement('div');
+        // creazione elemento HTML per le celle 
+        const eleCell = document.createElement('div');
             
-//         // creazione di un elemento nuovo
-//         eleCell.classList.add('cell');
+        // creazione di un elemento nuovo
+        eleCell.classList.add('cell');
         
-//         // mettere un contenuto all'interno di un elemento
-//         eleGrid.append(eleCell);
+        // mettere un contenuto all'interno di un elemento
+        eleGrid.append(eleCell);
             
-//         // stampa dei numeri 
-//         eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
+        // stampa dei numeri 
+        eleCell.innerHTML += `<div class=".grid">${[i]}</div>`
         
-//         // funzione per cui se cliccli si colora collegato a CSS
-//         eleCell.addEventListener('click', function () {
-//             this.classList.toggle('active')
-//         });
-//     }
-// });
+        // funzione per cui se cliccli si colora collegato a CSS
+        eleCell.addEventListener('click', function () {
+            this.classList.toggle('active')
+        });
+    }
+});
 
 
+
+
+
+
+/*
+//PARTE 2
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
+PASSAGGI IN ITALIANO:
+
+1. creazione di 16 numeri casuali (le bombe), non ci possono essere numeri uguali
+2. SE si clicca su uno dei 16 numeri, abbiamo perso, ALTRIMENTI si continua
+3. 
+*/
 
